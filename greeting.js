46989dc569b1f764bcb8bd8326d3fee99289e7bb
waden10 <fantm10@hotmail.com>
2017-04-27 17:52:31 +0000

    greeting script

---
+++ greeting.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting = {};
+
+if (hourNow > 17) {
+  greeting = 'Good Evening!'
+} else if (hourNow > 12 || hourNow < 16) {
+  greeting = 'Good Afternoon!';
+} else if (hourNow > 0 || hourNow < 12) {
+  greeting = 'Good Morning!';
+} else {
+  greetin = 'Welcome!';
+}
+
+console.log(greeting);
